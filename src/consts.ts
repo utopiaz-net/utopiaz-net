3fb424b25170bea252b0f4dd7ca39a0f94427c47
KpryKorn <roffinisacha@gmail.com>
2023-08-26 07:03:35 +0200

    Created "getAllPosts" function to GET all blog posts and sort them by most recent one

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+import { getCollection } from "astro:content";
 
 export const SITE_TITLE = "Utopiaz";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+
+// Get all `src/content/blog/` entries and sort them by the most recent one
+export const getAllPosts = (await getCollection("blog")).sort(
+  (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+);
